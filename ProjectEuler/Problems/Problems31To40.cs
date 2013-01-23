4fb6c6a09940816e552d077506ce5ba086471d6d
Phil Standen <philip_standen@hotmail.co.uk>
2013-01-23 13:34:50 +0000

    Problem 40 (non mathematical)

--- ProjectEuler/Problems/Problems31To40.cs
+++ ProjectEuler/Problems/Problems31To40.cs
@@ -13,5 +13,35 @@ namespace ProjectEuler
                 OrderByDescending(x => x.Item2).
                 First().Item1;
         }
+
+        public int Problem40()
+        {
+            /*
+            int x = 100;
+
+            int mult = 1;
+            int start = 9;
+            int oldStart = start;
+            while (x > start)
+            {
+                int thing = start * MathsHelper.Power(10, mult);
+                mult++;
+                oldStart = start;
+                start = start + (thing*mult);
+            }
+
+            int num = x - oldStart - 1;
+            int f = MathsHelper.Power(10, mult - 1) + (num/mult);
+            int index = num%mult;
+
+            int answer = Int32.Parse(f.ToString()[index].ToString());
+            */
+
+            //not mathematical
+            var t = Enumerable.Range(1, 1000000).SelectMany(x => x.ToString()).ToArray();
+            var indexes = Enumerable.Range(0, 7).Select(x => MathsHelper.Power(10, x));
+            return indexes.Select(x => Int32.Parse(t[x - 1].ToString())).Product();
+
+        }
     }
 }
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -11,7 +11,7 @@ namespace ProjectEuler
         static void Main(string[] args)
         {
             Problems p = new Problems();
-            p.Problem39();
+            p.Problem40();
         }
     }
 }
